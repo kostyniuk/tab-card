46e228bf75748c11e433d4ad43404148c55471e7
a.kostyniuk <alex.kostyniuk@amfg.ai>
2026-04-23 17:51:40 +0300

    kinda composed + add ts support

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { TabCard } from "@/components/custom/tab-card"
+import { TAB_CARD_EXAMPLE_PROPS } from "@/components/custom/tab-card.example"
 
 export default function Page() {
   return (
     <div className="flex min-h-svh p-6">
-      <TabCard />
+      <TabCard tabs={TAB_CARD_EXAMPLE_PROPS} />
     </div>
   )
 }
